fede9479bffb847d660a49935fe34f77f52c9a38
Adel <80000156+Cyberft-pdf@users.noreply.github.com>
2023-10-07 22:45:41 +0200

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -3,25 +3,30 @@ import cmath
 
 
 
-a = int(input("kvadratický člen: "))
+a = int(input("quadratic term: "))
 
-b = int(input("linearní člen: "))
+b = int(input("linear term: "))
 
-c = int(input("absolutní člen: "))
+c = int(input("absolute term: "))
 
 disk = (b**2) - (4*a*c)
 
 
-if disk > 0:
-    print("Tato kvadratická rovnice nemá řešení")
+if disk < 0:
+    print("This quadratic equation has no solution")
+
+if disk >0:
+    sol1 = (-b-cmath.sqrt(disk))/(2*a)
+    sol2 = (-b+cmath.sqrt(disk))/(2*a)
+
+    print(f"result: {sol1},{sol2}")
 
 
 if disk == 0:
-     odpovet = input("Rovnice má 1 řešení, chceš ho znát?:")
-     if odpovet == "ano" :
-          sol1 = (-b-cmath.sqrt(disk))/(2*a)
-          sol2 = (-b+cmath.sqrt(disk))/(2*a)
+     odpovet = input("The equation has 1 solution, do you want to know it?:")
+     if odpovet == "yes":
+          sol3 = (-b)/(2*a)
           
-          print(f"dva kořeny jsou {sol1},{sol2}")
+          print(f"result: {sol3}")
 
 #made by cyberft-pdf
